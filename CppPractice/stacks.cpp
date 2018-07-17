7c2c150b9928b00e9244b29f77a5fc68aa684518
agarwalanant <agarwal_anant@outlook.com>
2018-07-17 12:54:05 +0530

    update stack.cpp

--- CppPractice/stacks.cpp
+++ CppPractice/stacks.cpp
@@ -2,10 +2,35 @@
 
 using namespace std;
 
-public class node(int data, int* address)
+class Node()
 {
-	
+	int data;
+	Node next;
 }
+
+
+public pop(Node node)
+{
+	int data = Node.data;
+
+	node=node.next;
+
+	return data;
+}
+
+
+public push(Node node, int data)
+{
+	Node old = node;
+
+	head = new Node();
+
+	head.data=data;
+
+	head.next = old;
+}
+
+
 
 int main(int argc, char const *argv[])
 {
