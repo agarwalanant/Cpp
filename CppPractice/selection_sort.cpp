3fc87bccfd19b52f9f4e7e5c9c6c580d0b707723
agarwalanant <agarwal_anant@outlook.com>
2018-07-18 23:30:02 +0530

    update selection sort

---
+++ CppPractice/selection_sort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include<array>
+#include<iterator>
+using namespace std;
+
+
+void swap(int *first, int *second)
+{
+	int temp = *first;
+	*first = *second;
+	*second = temp;
+}
+
+void sort(int data[])
+{
+	int length = sizeof(p) / sizeof(p[0]);
+
+	for (int i = 0; i < length; ++i)
+	{
+		 int min =i;
+		for (int j = i+1; j < length; ++j)
+		{
+			if(data[j]<data[min])
+			{
+				min = j;
+
+			}
+			swap(&data[i],&data[j]);
+		}
+	}
+}
+
+
+int main(int argc, char const *argv[])
+{
+	int arr[]= {5,4,3,2,1};
+	sort(arr);
+	for(int i =0 ;i < 5;i++)
+	{
+        cout<<arr[i];
+	}
+	return 0;
+}
