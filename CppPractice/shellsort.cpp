debdd6edb1788241b672b0657614ae372fb90147
agarwalanant <agarwal_anant@outlook.com>
2018-07-19 00:43:38 +0530

    shellsort v1.0

--- CppPractice/shellsort.cpp
+++ CppPractice/shellsort.cpp
@@ -7,26 +7,38 @@ void swap(int *first, int *second)
 	*first = *second;
 	*second = temp;
 }
-
-
-void sort(int data[],int length)
+void insertsort(int data[],int length,int h)
 {
 	for (int i = 0; i < length; i++)
 	{
 		int j =i;
 		while(data[j]<data[j-1])
 		{
-			//swap(&data[j],&data[j-1]);
-			j--;
+			swap(&data[j],&data[j-1]);
+			j -=h;
+
+			if (j <0)
+			{
+				break;
+			}
 		}
-		swap(&data[j],&data[i]);
 	}
 }
+
+void shell(int data[],int length)
+{
+	insertsort(data,length,13);
+	insertsort(data,length,8);
+	insertsort(data,length,5);
+	insertsort(data,length,1);
+}
+
+
 
 int main(int argc, char const *argv[])
 {
 		int arr[]= {5,4,3,2,1};
-	sort(arr,5);
+	shell(arr,5);
 	for(int i =0 ;i < 5;i++)
 	{
         cout<<arr[i];
