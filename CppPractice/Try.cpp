ed6d3209698ab36a35ab27d276c70352de094c29
agarwalanant <agarwal_anant@outlook.com>
2018-07-19 15:46:19 +0530

    "code revision"

--- CppPractice/Try.cpp
+++ CppPractice/Try.cpp
@@ -7,12 +7,12 @@ vector<int> v;
 
 bool find(int first,int sencond)
 {
-	return (v[first]== v[sencond])
+	return (v[first]== v[sencond]);
 
 }
 
 
-void union(int first,int sencond)
+void un(int first,int sencond)
 {
 	for (int i = 0; i < v.size(); ++i)
 	{
@@ -22,22 +22,22 @@ void union(int first,int sencond)
 		}
 	}
 
-	
+
 }
 
-bool populate(
+bool populate()
 {
 	for (vector<int>::iterator i = v.begin(); i != v.end(); ++i)
 	{
-		v.push_back(i);
+//		v.push_back(i)
 	}
 }
 
 
-void main(int argc, char const *argv[])
+int main(int argc, char const *argv[])
 {
 
 	std::vector<int> v;
-	
+
 	return 0;
 }
--- CppPractice/stacks_array_implementation.cpp
+++ CppPractice/stacks_array_implementation.cpp
@@ -1,51 +1,51 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-class stack
-{
-private:
-	char s[];
-	int N =0;
-public:
-	void fixed_stack(int capacity)
-	{
-		 s[capacity];
-	}
-
-	bool push(string data)
-	{
-		s[N++] = data;
-		return 1;
-	}
-
-	int pop()
-	{
-		 if(N==0)
-		 {
-		 	cout<<"Empty Stack"<<endl;
-			return 0;
-		 }
-		 else
-		 {
-		 	return s[--N];
-		 }
-
-	}
-
-	bool isEmpty()
-	{
-		if(N==0)
-			return 1;
-		else
-			return 0;
-	}
-
-};
-
-int main(int argc, char const *argv[])
-{
-	/* code */
-	return 0;
-}
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//class stack
+//{
+//private:
+//	char s[];
+//	int N =0;
+//public:
+//	void fixed_stack(int capacity)
+//	{
+//		 s[capacity];
+//	}
+//
+//	bool push(string data)
+//	{
+//		s[N++] = data;
+//		return 1;
+//	}
+//
+//	int pop()
+//	{
+//		 if(N==0)
+//		 {
+//		 	cout<<"Empty Stack"<<endl;
+//			return 0;
+//		 }
+//		 else
+//		 {
+//		 	return s[--N];
+//		 }
+//
+//	}
+//
+//	bool isEmpty()
+//	{
+//		if(N==0)
+//			return 1;
+//		else
+//			return 0;
+//	}
+//
+//};
+//
+//int main(int argc, char const *argv[])
+//{
+//	/* code */
+//	return 0;
+//}
