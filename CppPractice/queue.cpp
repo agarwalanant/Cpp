bc4caef7f22d90815dc579797e6bc36125fbec34
agarwalanant <agarwal_anant@outlook.com>
2018-07-25 23:24:01 +0530

    final code update

--- CppPractice/queue.cpp
+++ CppPractice/queue.cpp
@@ -1,82 +1,151 @@
-//
-// Created by Anant Agarwal on 7/25/18.
-//
-
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
-class Node
-{
-public:
+// Define node pointer
+struct node {
     int data;
-    Node* next;
+    node* next;
 };
 
-class Queue
-{
-public:
-    Node* top = nullptr;
-    Node* end= nullptr;
-    bool push(int);
-    int pop();
-    bool is_empty();
-    int count =0;
-};
+// Head and tail pointers
+node* head;
+node* tail;
 
-bool Queue::push(int data)
-{
-    auto node = new (Node);
-    node->data = data;
-    node->next = nullptr;
-    if(top == nullptr)
-    {
-        top = end = node;
-        end->next = NULL;
-    } else
-    {
-        end->next = node;
-        end = node;
+// Functions
+int menu();
+void enqueue(node *& head, node *&tail, int data);
+void dequeue(node *&head, node *& tail);
+void front(node *& head);
+bool isEmpty(node *& head);
+int randomNumber(int min, int max);
+
+//Main function
+int main() {
+
+    node* head;
+    node* tail;
+
+    head = NULL;
+    tail = NULL;
+    int choice;
+    printf("Number is %d", randomNumber(0, 10));
+    do {
+        choice = menu();
+
+        switch(choice) {
+            case 1:
+                int data;
+                cout<<"Enter value to be stored in queue: ";
+                cin>>data;
+                enqueue(head,tail,data);
+                break;
+            case 2:
+                dequeue(head,tail);
+                break;
+            case 3:
+                front(head);
+                break;
+            case 4:
+                printf("Not implemented");
+                break;
+            case 5:
+                isEmpty(head);
+                break;
+            case -1:
+                choice = -1;
+                break;
+
+            default: printf("\nEnter a valid choice!!");
+        }
+    } while(choice != -1);
+
+    return 0;
+}
+
+//Menu function
+int menu() {
+    int ch;
+    printf("\nStack");
+    printf("\n1.Enqueue\n2.Dequeue\n3.Front element\n4.Is Full\n5.Is Empty\n6.Exit");
+    printf("\nEnter your Choice:");
+    scanf("%d",&ch);
+    return ch;
+}
+
+//Enqueue element in stack
+void enqueue(node *& head, node *&tail, int data) {
+    if (head == NULL) {
+        node* n= new node;
+
+        n->data = data;
+        n->next = NULL;
+
+        head = n;
+        tail = n;
+
+    } else {
+        node* n = new node;
+        n->data = data;
+        n->next = head;
+        head = n;
     }
-
 }
 
+// Dequeue first element in a queue
+void dequeue(node *&head, node *& tail) {
+    if (head == NULL) {
+        printf("\nThe queue is empty.");
 
+    } else if (head == tail) {
+        // Case when there is only 1 element in the stack
+        printf("The value %d was dequeude", head->data);
+        delete head;
+        head = NULL;
+        tail = NULL;
 
-
-int Queue::pop() {
-        if (top == end)
-        {
-            cout<<"Empty Queue "<<endl;
-//            top = end = nullptr;
-
-            return 0;
+    } else {
+        node* n = new node;
+        n = tail;
+        if(tail->next == NULL) {
+            tail = head;
+        } else {
+            tail = tail->next;
         }
-        else
-        {
-            int data = top->data;
-            Node* temp = top;
-            top = top->next;
-            delete(temp);
-            return data;
 
-        }
-
+        printf("\nThe value %d was dequeued", n->data);
+        delete n;
+    }
 }
 
-bool Queue::is_empty() {
-    if(top== end)
+//Show top-most element in stack
+void front(node *& head) {
+    if (head == NULL) {
+        printf("\nThe queue if empty");
+
+    } else {
+        printf("\nFront number is %d", head->data);
+    }
+}
+
+// Shows if the stack is empty
+bool isEmpty(node *& head) {
+    if(head == NULL) {
+        printf("\nThe queue is empty");
         return true;
-    else
+
+    } else {
+        printf("\nThe queue is not empty");
         return false;
+    }
 }
 
-int main(int argc, char const *argv[]) {
-    auto s1 = new (Queue);
-    s1->push(0);
-    s1->push(3);
-
-    cout << "popped " << s1->pop() << endl;
-    cout<< s1->pop();
-    s1->pop();
+int randomNumber(int min, int max) {
+    static bool first = true;
+    if ( first )
+    {
+        srand(time(NULL));
+        first = false;
+    }
+    return min + rand() % (max - min);
 }
