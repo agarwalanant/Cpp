a6da9159a358c2c29b79b8d9ea6cdc657f81846c
agarwalanant <agarwal_anant@outlook.com>
2018-08-05 17:09:12 +0530

    binary search tree start

---
+++ CppPractice/BST.cpp
@@ -0,0 +1,54 @@
+//
+// Created by Anant Agarwal on 8/5/18.
+//
+
+#include "iostream"
+
+using namespace std;
+
+
+
+class BST
+{
+private: class Node
+    {
+    private:
+        long key = INT64_MIN, value = INT64_MIN;
+        Node* left = nullptr;
+        Node* right = nullptr;
+    public:
+        Node(long key,long value)
+        {
+            this->value = value;
+            this->key = key;
+        }
+
+    };
+private: Node root;
+
+public:
+    void put(long key, long value)
+    {
+        fdopen()
+    }
+
+    long get(long key)
+    {
+
+    }
+
+    void del(long key)
+    {
+
+    }
+
+
+};
+
+
+int main()
+{
+
+
+    return 0;
+}
