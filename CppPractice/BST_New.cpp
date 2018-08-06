c85e0ecbe8218b843add431dc29542d6a5fece01
agarwalanant <agarwal_anant@outlook.com>
2018-08-06 23:11:34 +0530

    Full BST Implemented in BST_New

--- CppPractice/BST_New.cpp
+++ CppPractice/BST_New.cpp
@@ -4,19 +4,7 @@
 #include<iostream>
 using namespace std;
 //Definition of Node for Binary search tree
-class BstNode {
-public:
-    int data;
-    BstNode* left;
-    BstNode* right;
 
-public:  BstNode(int data)
-    {
-        this->data = data;
-        this->right = right;
-        this->left = left;
-    }
-};
 
 
 
@@ -26,7 +14,25 @@ public:  BstNode(int data)
 
 class BST
 {
-    BstNode* root;
+public: BST()
+    {
+        root = nullptr;
+    }
+    class BstNode {
+    public:
+        int data;
+        BstNode* left;
+        BstNode* right;
+
+    public:  BstNode(int data)
+        {
+            this->data = data;
+            this->right = right;
+            this->left = left;
+        }
+    };
+public:BstNode* root;
+
 
 BstNode* Insert(BstNode* root,int data) {
     if(root == nullptr) { // empty tree
@@ -40,12 +46,13 @@ BstNode* Insert(BstNode* root,int data) {
     else {
         root->right = Insert(root->right,data);
     }
+
     return root;
 }
 
-public:BstNode* Insert(int data)
+public:void Insert(int data)
 {
-    Insert(root,data);
+   root = Insert(root,data);
 }
 //To search an element in BST, returns true if element is found
 bool Search(BstNode* root,int data) {
@@ -70,6 +77,60 @@ public: bool Search(int data)
         return true;
     else return false;
 }
+
+public: BstNode* Delete(BstNode* node, int data)
+{
+    BstNode* temp;
+    if(root == nullptr) {return root;}
+    else if(data < root->data) root->left =  Delete(root->left,data);
+    else if(data > root->data) root->right = Delete(root->right,data);
+
+    else if(root->left && root->right)
+    {
+        temp = min(root->right);
+
+        root->data = temp->data;
+        root->right = Delete(root->right,temp->data);
+    }
+    else
+    {
+        temp = root;
+        if(root->left == nullptr && root->right == nullptr)
+        {
+            delete root;
+            root = nullptr;
+        }
+
+        else if(root->left == nullptr)
+        {
+             temp = root;
+            root = root->right;
+            delete temp;
+        }
+
+        else if( root->right == nullptr)
+        {
+             temp = root;
+            root = root->left;
+            delete temp;
+        }
+    }
+
+    return root;
+
+
+}
+
+public: void Delete(int data)
+{
+   root =  Delete(root,data);
+}
+
+BstNode* min(BstNode* root)
+{
+    while (root->left != nullptr) root = root->left;
+    return root;
+}
 };
 int main() {
       // Creating an empty tree
@@ -81,11 +142,13 @@ int main() {
     b.Insert(25);
    b.Insert(8);
     b.Insert(12);
-
+//   b.Delete(25);
     int number;
     cout<<"Enter number be searched\n";
     cin>>number;
 
     if(b.Search(number)) cout<<"Found\n";
     else cout<<"Not Found\n";
+    b.Delete(25);
+
 }
