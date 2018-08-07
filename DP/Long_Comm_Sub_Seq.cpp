0a1e328059815a8616e3f70d4bdc5909d5303b31
agarwalanant <agarwal_anant@outlook.com>
2018-08-08 00:09:51 +0530

    Long Common Sub seq update

--- DP/Long_Comm_Sub_Seq.cpp
+++ DP/Long_Comm_Sub_Seq.cpp
@@ -13,8 +13,8 @@ int lcs(char *X, char *Y, int m, int n)
     int l[m+1][n+1];
     int i, j;
 
-    for (int i = 0; i <m ; ++i) {
-        for (int j = 0; j < n ; ++j) {
+    for ( i = 0; i <m ; ++i) {
+        for ( j = 0; j < n ; ++j) {
 
             if (i ==0 || j== 0 )
                 l[i][j] =0;
